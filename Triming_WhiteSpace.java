b53ef941a8615aa5a2a89197ea2680e0f5525cd0
Purav Desai <puravdesai57@gmail.com>
2018-03-24 18:24:47 -0700

    Add files via upload

---
+++ Triming_WhiteSpace.java
@@ -0,0 +1,12 @@
+public class Exercise {
+    
+   public static void main(String[] args)
+    {
+        String str = "The quick brown fox jumps over the lazy dog.";
+
+        String new_str = str.substring(10, 26);
+
+        System.out.println("old = " + str);
+        System.out.println("new = " + new_str);
+    }
+}
